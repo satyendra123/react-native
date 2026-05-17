0dfdd4d41e2b62f4e5e23297a96e4d6a2baa4133
satyendra123 <satyendra123@users.noreply.github.com>
2026-05-17 14:54:54 +0530

    Add NativeWind styling setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
+import "react-native-reanimated";
+
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Text>Welcome to Expo Router!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
